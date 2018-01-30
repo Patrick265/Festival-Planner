042c615acf669c478566f0490a4d624dc26db768
Unknown <thomas.mandemaker@hotmail.com>
2018-01-30 10:24:25 +0100

    Test voor Master push

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,8 @@
  * @Author Thomas Mandemaker, Patrick de Jong, Yannick van Dolen , Sergen Peker , Anastasia Hellemons
  * @version 1.0
  */
-<<<<<<< Updated upstream
-public class Main
-{
-    public static void main(String[] args)
-    {
-        superCooleMethode();
-    }
-    
-    private static void superCooleMethode() {
-        System.out.println("Dit is echt cool");
-=======
 public class Main {
     public static void main(String[] args) {
-        // hoi patrick
-        //Test
->>>>>>> Stashed changes
+        //test
     }
 }
