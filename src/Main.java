728710b10a9b61a509e5f3f78cdfb8a54c0dc56c
Patrick de Jong <Patrick.dejong@kpnmail.nl>
2018-01-30 09:59:28 +0100

    [Added] A Main file added, Not all names are added to main file

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @Author Thomas Mandemaker, Patrick de Jong, Yannick .... , Sergen ..... , Anastasia .....
+ * @version 1.0
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+    }
+}
+    
