8fc5f71419d3aa6dc97467c3f6f551c27893a7d2
Unknown <thomas.mandemaker@hotmail.com>
2018-01-30 10:23:10 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
  * @Author Thomas Mandemaker, Patrick de Jong, Yannick van Dolen , Sergen Peker , Anastasia Hellemons
  * @version 1.0
  */
+<<<<<<< Updated upstream
 public class Main
 {
     public static void main(String[] args)
@@ -11,5 +12,11 @@ public class Main
     
     private static void superCooleMethode() {
         System.out.println("Dit is echt cool");
+=======
+public class Main {
+    public static void main(String[] args) {
+        // hoi patrick
+        //Test
+>>>>>>> Stashed changes
     }
 }
