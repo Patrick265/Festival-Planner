61546b8037e43a1af1603d4d5b0deaec1f6a1f7b
Unknown <amchelle@avans.nl>
2018-02-06 10:32:54 +0100

    Artist klasse toegevoegd
    de artist klasse heeft op het moment 3 get methodes en een set methode

---
+++ src/logica/Artist.java
@@ -0,0 +1,37 @@
+package logica;
+
+import java.io.File;
+
+public class Artist
+{
+    private String name;
+    private File photo;
+    private String genre;
+
+    public void Artist(String name, File photo, String genre)
+    {
+        this.name = name;
+        this.photo = photo;
+        this.genre = genre;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getGenre()
+    {
+        return genre;
+    }
+
+    public File getPhoto()
+    {
+        return photo;
+    }
+
+    public void changePhoto(File newPhoto)
+    {
+        photo = newPhoto;
+    }
+}
