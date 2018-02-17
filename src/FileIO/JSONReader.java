cce3e36e81b651a33b9104c6a3c27701bc2a61b4
Unknown <thomas.mandemaker@hotmail.com>
2018-02-17 19:39:06 +0100

    Re-upload

--- src/FileIO/JSONReader.java
+++ src/FileIO/JSONReader.java
@@ -7,7 +7,7 @@ import java.io.File;
 public class JSONReader {
     public static void main(String[] args) throws Exception{
         ObjectMapper objectMapper = new ObjectMapper();
-        File file = new File("data/artists.json");
+        File file = new File("Festival-Planner/src/FileIO/artists.json");
         Artiest artist = objectMapper.readValue(file, Artiest.class);
         System.out.println(artist.getName());
         System.out.println(artist.getGenre());
